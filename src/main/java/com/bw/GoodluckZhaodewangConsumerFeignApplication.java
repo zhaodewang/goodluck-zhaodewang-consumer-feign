d0b9a93cc79910ee3b634e6922d7ebe2264453a6
赵德旺 <972525673@qq.com>
2017-07-28 18:48:39 +0800

    Initial commit

---
+++ src/main/java/com/bw/GoodluckZhaodewangConsumerFeignApplication.java
@@ -0,0 +1,14 @@
+package com.bw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class GoodluckZhaodewangConsumerFeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodluckZhaodewangConsumerFeignApplication.class, args);
+	}
+}
---
+++ src/main/java/com/bw/contrilo/DcController.java
@@ -0,0 +1,23 @@
+package com.bw.contrilo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by 赵德旺 on 2017/7/24.
+ */
+@RestController
+public class DcController {
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    @GetMapping("/dc")
+    public String dc() {
+        String services = "Services: " + discoveryClient.getServices();
+        System.out.println(services);
+        return services;
+    }
+
+}
